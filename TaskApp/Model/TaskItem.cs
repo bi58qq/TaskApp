568a92fabee9e76c40cc78d675b700902512708d
bi58qq <bi58qq@student.sunderland.ac.uk>
2025-12-05 21:59:38 +0545

    Added models

---
+++ TaskApp/Model/TaskItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApp.Model
+{
+    public enum TaskStatus
+    {
+        ToDo,
+        InProgress,
+        Done
+    }
+
+    public enum TaskPriority
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public class TaskItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public TaskPriority Priority { get; set; }
+        public string Assignee { get; set; }
+        public TaskStatus Status { get; set; } = TaskStatus.ToDo;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    }
+}
---
+++ TaskApp/Model/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TaskApp.Model
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
